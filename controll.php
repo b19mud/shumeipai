7ca697e4547ab1b082f96b3968ced0dbdea97af5
bigmud <lihaijianbaojian@163.com>
2017-09-13 15:27:34 +0800

    second

--- controll.php
+++ controll.php
@@ -5,8 +5,13 @@ if (!empty($_POST['guanjiao']) && !empty($_POST['id'])){
 
 	if (($a>=1 && $a <= 26) && ($b=1 || $b = 0))
 	{
-	system("./controll.sh".' '.$a.' '.$b);
-	echo "<meta http-equiv=\"refresh\" content=\"1;url=index.php\"><body>success</body>";
+		system("./controll.sh".' '.$a.' '.$b,$status);
+		if ($status = 0){
+			echo "<meta http-equiv=\"refresh\" content=\"1;url=index.php\"><body>success</body>";
+		}
+		else{
+			echo "<meta http-equiv=\"refresh\" content=\"1;url=index.php\"><body>something error</body>";
+		}
 	}
 	else
 	{
